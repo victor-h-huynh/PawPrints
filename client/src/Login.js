3c1e0156a29503f9d987c82805e35936caaac0ec
JoshuaHarris93 <josh@emptychairproductions.com>
2019-06-21 16:19:41 +0000

    sass install

---
+++ client/src/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    render(){
+        return(
+            <div className="login">
+                <textarea type=
+            </div>
+        )
+    }
+}
+
+export default Login;
