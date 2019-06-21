62283cb1a368bf6ecbd79b4297aa8a80d01fef2d
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-21 23:24:58 +0000

    Now renders pet's name, description, status on PetProfile page

--- client/src/PetProfile.js
+++ client/src/PetProfile.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import TimeAgo from 'react-timeago';
 
 class PetProfile extends Component {
 
     getPet() {       
         const id = Number(this.props.match.params.id);
         const pets = this.props.pets;
-        const pet = pets.filter(pet => pet.id === id);
-        return pet[0];
+        const [pet] = pets.filter(pet => pet.id === id);
+        return pet;
     } 
-
-    componentDidMount() {
-        const id = Number(this.props.match.params.id);
-        const pets = this.props.pets;
-        const pet = pets.filter(pet => pet.id === id);
-        console.log("PROP", pets, pet, id);
-    }
 
     render() {
         const pet = this.getPet();
         return (
-            <div className="Pet">
-           <h1>{pet.name}</h1>
-          </div>
-  
+            <Card key="{pet.id}">
+            <Card.Header>{pet.name}</Card.Header>
+            <Card.Img variant="top" src={pet.picture} />
+            <Card.Body>
+              <Card.Title>{pet.name}, a {pet.status} {pet.species} in {pet.address.city}, {pet.address.postal_code}</Card.Title>
+              <Card.Title className="StatusIcon">{pet.status}</Card.Title>
+              <Card.Text>
+                <p>Breed: {pet.description.breed}</p>
+                <p>Colour: {pet.description.colour}</p>
+                <p>Sex: {pet.description.sex}</p>
+                <p>{pet.status} <TimeAgo date={pet.date_lost}/></p>
+                <p>Additional description: {pet.description.additional}</p>
+              </Card.Text>
+              <hr></hr>
+              <Card.Text>
+                  <p>{pet.name} was {pet.status.toLowerCase()} near {pet.address.street_name}, {pet.address.city}</p>
+              </Card.Text>
+              <hr></hr>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
       );
     }
 };
