c0ddbc4eb415c66ef0ccc9cbb0edbc309f033dd8
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-22 22:11:37 +0000

    Added state management to get form input

--- client/src/ReportAPet.js
+++ client/src/ReportAPet.js
@@ -1,63 +1,90 @@
-import React, { Component } from "react";
-import { Form, Button, Col } from "react-bootstrap";
+import React, { Component } from 'react';
+import { Form, Button, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 class ReportAPet extends Component {
+  state = {
+    selectedFile: null,
+    name: '',
+    species: '',
+    status: '',
+    date_lost: '',
+    picture: './src/assets/catImage.jpg',
+    user_id: '',
+    breed: '',
+    colour: '',
+    sex: '',
+    additional: '',
+    street_number: '',
+    street_name: '',
+    apartment: '',
+    city: '',
+    province: '',
+    postal_code: '',
+    latitude: 45.7,
+    longitude: -73.1,
+  };
 
-  constructor(props) {
-    super(props);
+  fileSelectedHandler = event => {
+    console.log(event.target.files[0]);
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
-  handleSubmit(event) {
+  handleSubmit = event => {
     event.preventDefault();
-    console.log("form submitted");
-    axios.post("http://localhost:3001/api/pets", {
-      description: {
-        breed: "mutt",
-        colour: "orange",
-        sex: "male",
-        additional: "Very fat boy",
-      },
-      address: {
-        street_number: 11,
-        street_name: 'Sherlock St',
-        apartment: '123A',
-        city: 'Montreal',
-        province: 'Quebec',
-        postal_code: "H2s0A7", 
-        latitude: 45.70,
-        longitude: -73.1,
-      },
-        name: "Tom",
-        species: "Cat",
-        status: "Lost",
-        date_lost: Date.now(),
-        picture: "./src/assets/catImage.jpg",
+    axios
+      .post('http://localhost:3001/api/pets', {
+        description: {
+          breed: this.state.breed,
+          colour: this.state.colour,
+          sex: this.state.sex,
+          additional: this.state.additional,
+        },
+        address: {
+          street_number: this.state.street_number,
+          street_name: this.state.street_name,
+          apartment: this.state.apartment,
+          city: this.state.city,
+          province: this.state.province,
+          postal_code: this.state.postal_code,
+          latitude: 45.7,
+          longitude: -73.1,
+        },
+        name: this.state.name,
+        species: this.state.species,
+        status: this.state.status,
+        date_lost: this.state.date,
+        picture: this.state.picture,
         user_id: 1,
-    })
-    .then(response => {
-      console.log('response: ', response);
-    })
-    .catch(err => {
-      console.log('report pet error: ', err);
-    })
-  }  
+      })
+      .then(response => {
+        console.log('response: ', response);
+      })
+      .catch(err => {
+        console.log('report pet error: ', err);
+      });
+  };
 
   render() {
     return (
-      
       <Form onSubmit={this.handleSubmit}>
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridName">
+          <Form.Group as={Col} controlId='formGridName'>
             <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Enter name" />
+            <Form.Control type='name' name="name" placeholder='Enter name' value={this.state.name} onChange={this.handleChange}/>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridSpecies">
+          <Form.Group as={Col} controlId='formGridSpecies'>
             <Form.Label>Species</Form.Label>
-            <Form.Control as="select">
+            <Form.Control as='select' name="species" value={this.state.species} onChange={this.handleChange}>
               <option>Enter a Species</option>
               <option>Cat</option>
               <option>Dog</option>
@@ -68,56 +95,60 @@ class ReportAPet extends Component {
             </Form.Control>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridBreed">
+          <Form.Group as={Col} controlId='formGridBreed'>
             <Form.Label>Breed</Form.Label>
-            <Form.Control type="breed" placeholder="Enter Breed" />
+            <Form.Control type='breed' name="breed" placeholder='Enter Breed' value={this.state.breed} onChange={this.handleChange}/>
           </Form.Group>
         </Form.Row>
 
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridStatus">
+          <Form.Group as={Col} controlId='formGridStatus'>
             <Form.Label>Status</Form.Label>
-              <Form.Control as="select">
-                <option>Status</option>
-                <option>Lost</option>
-                <option>Found</option>
-                <option>Reunited</option>
-              </Form.Control>
+            <Form.Control as='select' name="status" value={this.state.status} onChange={this.handleChange}>
+              <option>Status</option>
+              <option>Lost</option>
+              <option>Found</option>
+              <option>Reunited</option>
+            </Form.Control>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridStatus">
-            <Form.Label>Gender</Form.Label>
-              <Form.Control as="select">
-                <option>Gender</option>
-                <option>Male</option>
-                <option>Female</option>
-              </Form.Control>
+          <Form.Group as={Col} controlId='formGridStatus'>
+            <Form.Label>Sex</Form.Label>
+            <Form.Control as='select' name="sex" value={this.state.sex} onChange={this.handleChange}>
+              <option>Sex</option>
+              <option>Male</option>
+              <option>Female</option>
+            </Form.Control>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridColoir">
+          <Form.Group as={Col} controlId='formGridColoir'>
             <Form.Label>Colour</Form.Label>
-            <Form.Control type="name" placeholder="Colour" />
+            <Form.Control type='name' name="colour" placeholder='Colour' value={this.state.colour} onChange={this.handleChange}/>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridDateLost">
+          <Form.Group as={Col} controlId='formGridDateLost'>
             <Form.Label>Date Lost</Form.Label>
-            <Form.Control type="name" placeholder="Date Lost" />
+            <Form.Control type='name' name="date_lost" placeholder='Date Lost' value={this.state.date_lost} onChange={this.handleChange}/>
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridLastSeen">
+          <Form.Group as={Col} controlId='formGridLastSeen'>
             <Form.Label>Last known location</Form.Label>
-            <Form.Control placeholder="Last Seen"/>
+            <Form.Control placeholder='Last Seen' />
           </Form.Group>
         </Form.Row>
 
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridAdditionalComments">
+          <Form.Group as={Col} controlId='formGridAdditionalComments'>
             <Form.Label>Additional Comments</Form.Label>
-            <Form.Control placeholder=""/>
+            <Form.Control placeholder='' name="additional" value={this.state.additional} onChange={this.handleChange}/>
           </Form.Group>
         </Form.Row>
 
-        <Button variant="primary" type="submit">
+        <Form.Row>
+          <input type='file' onChange={this.fileSelectedHandler} />
+        </Form.Row>
+
+        <Button variant='primary' type='submit'>
           Submit
         </Button>
       </Form>
