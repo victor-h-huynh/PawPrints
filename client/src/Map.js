fc0c20fc38a0e85f74648aba3b093facd0459663
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-06-22 12:32:27 -0400

    Fix markers all showing when clicking on one

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,16 +1,5 @@
-
 import React, { Component } from "react";
 import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from "react-google-maps";
-import { compose, withProps, withStateHandlers } from "recompose"
-
-
-
-const mapStyles = {
-  width: '60vw',
-  height: '70vh'
-};
-
-
 
 
 class Map extends Component {
@@ -19,14 +8,17 @@ class Map extends Component {
     this.map = React.createRef();
   }
   state = {
-    pets: this.props.pets
+    pets: this.props.pets,
+    markerId: null
   }
 
-  //Make showINfo function returning InfoWindow
-  //OnClick => {}
+  onMarkerClick = (id) => {
+    this.setState({
+      markerId: id
+    })
+  }
 
-
-  renderMarkers(props2) {
+  renderMarkers() {
   return this.props.pets.map(pet => {
     return (
 
@@ -35,11 +27,11 @@ class Map extends Component {
       key={pet.id}
       position = {{lat: Number(pet.address.latitude), lng: -(Number(pet.address.longitude))}}
       name = {pet.name}
-      onClick={props2.onToggleOpen}
+      onClick={() => this.onMarkerClick(pet.id)}
       title = "test"
 
        >
-        {props2.isOpen && <InfoWindow onCloseClick={props2.onToggleOpen}>
+        {this.state.markerId === pet.id && <InfoWindow >
         <h4>{pet.name}</h4>
         </InfoWindow>}
          </Marker>
@@ -52,49 +44,35 @@ componentDidMount() {
 }
 
   render() {
-    const GoogleMapExample = compose(
-  withStateHandlers(() => ({
-    isOpen: false,
-  }), {
-    onToggleOpen: ({ isOpen }) => () => ({
-      isOpen: !isOpen,
-    })
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props2 => (
-        <GoogleMap
-          ref={map => {
-            this.map = map;
-          }}
-          onIdle={props2.onMapIdle}
-          defaultCenter={{ lat: 45.50, lng: -73.56 }}
-          defaultZoom={11}
-        >
-          {this.renderMarkers(props2)}
-        </GoogleMap>
-      ))
-
-    return (
-      <div>
-        <GoogleMapExample
-          onMapIdle={() => {
-            let ne = this.map.getBounds().getNorthEast();
-            let sw = this.map.getBounds().getSouthWest();
-            console.log(ne.lat() + ";" + ne.lng());
-            console.log(sw.lat() + ";" + sw.lng());
-          }}
-
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZo4EZUupTl2upWKh2W7xWD_12ixbr8PU"
-          loadingElement={<div style={{ height: `70vh`, width: `70vw` }} />}
-          containerElement={<div style={{ height: `70vh`, width: `70vw` }} />}
-          mapElement={<div style={{ height: `70vh`, width: `70vw` }} />}
 
 
-        />
-      </div>
-    );
+return (
+<MyMapComponent
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtBq7JJA3X8tnZT8n6HEPXuqSuDf4AsOQ"
+  loadingElement={<div style={{ height: `70vh`, width: `70vw`, margin: 'auto' }} />}
+  containerElement={<div style={{ height: `70vh`, width: `70vw`, margin: 'auto' }} />}
+  mapElement={<div style={{ height: `70vh`, width: `70vw`, margin: 'auto' }} />}
+
+>
+{this.renderMarkers()}
+</MyMapComponent>
+)
+
   }
+
 }
 
+
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={11}
+    defaultCenter={{ lat: 45.50, lng: -73.56 }}
+  >
+    {props.children}
+  </GoogleMap>
+))
+
+
 export default Map;
+
--- client/src/test.js
+++ client/src/test.js
@@ -2,11 +2,65 @@ import React, { Component } from 'react';
 import { GoogleApiWrapper, InfoWindow, Marker, Map } from 'google-maps-react';
 
 
+const mapStyles = {
+  width: '70vw',
+  height: '70vh',
+  margin: 'auto'
+};
 
 
 class MapContainer extends Component {
+   state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
 
+    onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
 
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  renderMarkers() {
+  return this.props.pets.map(pet => {
+    return <Marker
+
+      key={pet.id}
+      // onClick = { this.onMarkerClick }
+      position = {{lat: Number(pet.address.latitude), lng: -(Number(pet.address.longitude))}}
+      name = {pet.name}
+      onClick = {this.onMarkerClick}
+      icon={{url: 'https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg',
+        scaledSize: new this.props.google.maps.Size(60, 40)}}
+       >
+        <InfoWindow
+         marker={this.state.activeMarker}
+         visible={this.state.showingInfoWindow}
+         onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Marker>
+  })
+}
+
+  componentDidMount() {
+    console.log(this.props.google)
+  }
 
   render() {
     return (
@@ -16,10 +70,8 @@ class MapContainer extends Component {
         zoom={11}
         style={mapStyles}
         initialCenter={{ lat: 45.50, lng: -73.56 }}
-
       >
         {this.renderMarkers()}
-        {this.renderInfoWindow()}
 
       </Map>
       </div>
@@ -27,8 +79,6 @@ class MapContainer extends Component {
   }
 }
 
-
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY
 })(MapContainer);
-
