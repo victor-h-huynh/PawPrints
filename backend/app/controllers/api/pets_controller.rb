91454a164320e2ceb623199b7683cc6e35baf18f
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-22 20:20:21 +0000

    Re-enabled create

--- backend/app/controllers/api/pets_controller.rb
+++ backend/app/controllers/api/pets_controller.rb
@@ -4,7 +4,7 @@ class Api::PetsController < ApplicationController
       render :json => pets, :include=> [:address, :user, :description]
     end
 
-    # def create
+    def create
       # address = Address.create!(
       #   street_number: params['streetNumber'],
       #   street_name: params['streetName'],
@@ -13,23 +13,23 @@ class Api::PetsController < ApplicationController
       #   province: params['province'],
       #   postal_code: params['postalCode'], 
       # )
-      # description = Description.create!(
-      #   breed: params['description']['breed'],
-      #   colour: params['description']['colour'],
-      #   sex: params['description']['sex]'],
-      #   additional: params['description']['additional'],
-      # )
-    #   pet = Pet.create!(
-    #     name: params['name'],
-    #     species: params['species'],
-    #     status: params['status'],
-    #     date_lost: params['date'],
-    #     picture: params['picture'],
-    #     # address_id: address.id,
-    #     # description_id: description.id,
-    #     user_id: params['user'],
-    #   )
-    # end
+      description = Description.create!(
+        breed: params['description']['breed'],
+        colour: params['description']['colour'],
+        sex: params['description']['sex]'],
+        additional: params['description']['additional'],
+      )
+      pet = Pet.create!(
+        name: params['name'],
+        species: params['species'],
+        status: params['status'],
+        date_lost: params['date'],
+        picture: params['picture'],
+        # address_id: address.id,
+        description_id: description.id,
+        user_id: params['user'],
+      )
+    end
 
 end
 
