208caa6162561915f0dfbbda18052778e69530ed
Robin Westhaver <robinwesthaver@gmail.com>
2019-06-24 17:32:04 +0000

    trying to remove state and region

--- client/src/PetMap.js
+++ client/src/PetMap.js
@@ -11,9 +11,9 @@ class PetMap extends Component{
 		super( props );
 		this.state = {
 			address: '',
-			city: '',
-			area: '',
-			state: '',
+			// city: '',
+			// area: '',
+			// state: '',
 			mapPosition: {
 				lat: this.props.center.lat,
 				lng: this.props.center.lng
@@ -31,18 +31,18 @@ class PetMap extends Component{
 		Geocode.fromLatLng( this.state.mapPosition.lat , this.state.mapPosition.lng ).then(
 			response => {
 				const address = response.results[0].formatted_address,
-					addressArray =  response.results[0].address_components,
-					city = this.getCity( addressArray ),
-					area = this.getArea( addressArray ),
-					state = this.getState( addressArray );
+					addressArray =  response.results[0].address_components;
+					// city = this.getCity( addressArray ),
+					// area = this.getArea( addressArray ),
+					// state = this.getState( addressArray );
 
 				console.log( 'city', city, area, state );
 
 				this.setState( {
 					address: ( address ) ? address : '',
-					area: ( area ) ? area : '',
-					city: ( city ) ? city : '',
-					state: ( state ) ? state : '',
+					// area: ( area ) ? area : '',
+					// city: ( city ) ? city : '',
+					// state: ( state ) ? state : '',
 				} )
 			},
 			error => {
@@ -68,45 +68,45 @@ class PetMap extends Component{
 
 	// Get the city and set the city input value to the one selected
 
-	getCity = ( addressArray ) => {
-		let city = '';
-		for( let i = 0; i < addressArray.length; i++ ) {
-			if ( addressArray[ i ].types[0] && 'administrative_area_level_2' === addressArray[ i ].types[0] ) {
-				city = addressArray[ i ].long_name;
-				return city;
-			}
-		}
-	};
+	// getCity = ( addressArray ) => {
+	// 	let city = '';
+	// 	for( let i = 0; i < addressArray.length; i++ ) {
+	// 		if ( addressArray[ i ].types[0] && 'administrative_area_level_2' === addressArray[ i ].types[0] ) {
+	// 			city = addressArray[ i ].long_name;
+	// 			return city;
+	// 		}
+	// 	}
+	// };
 
 	//  Get the area and set the area input value to the one selected
 	 
-	getArea = ( addressArray ) => {
-		let area = '';
-		for( let i = 0; i < addressArray.length; i++ ) {
-			if ( addressArray[ i ].types[0]  ) {
-				for ( let j = 0; j < addressArray[ i ].types.length; j++ ) {
-					if ( 'sublocality_level_1' === addressArray[ i ].types[j] || 'locality' === addressArray[ i ].types[j] ) {
-						area = addressArray[ i ].long_name;
-						return area;
-					}
-				}
-			}
-		}
-	};
+	// getArea = ( addressArray ) => {
+	// 	let area = '';
+	// 	for( let i = 0; i < addressArray.length; i++ ) {
+	// 		if ( addressArray[ i ].types[0]  ) {
+	// 			for ( let j = 0; j < addressArray[ i ].types.length; j++ ) {
+	// 				if ( 'sublocality_level_1' === addressArray[ i ].types[j] || 'locality' === addressArray[ i ].types[j] ) {
+	// 					area = addressArray[ i ].long_name;
+	// 					return area;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// };
 
 	//   Get the address and set the address input value to the one selected
 	
-	getState = ( addressArray ) => {
-		let state = '';
-		for( let i = 0; i < addressArray.length; i++ ) {
-			for( let i = 0; i < addressArray.length; i++ ) {
-				if ( addressArray[ i ].types[0] && 'administrative_area_level_1' === addressArray[ i ].types[0] ) {
-					state = addressArray[ i ].long_name;
-					return state;
-				}
-			}
-		}
-	};
+	// getState = ( addressArray ) => {
+	// 	let state = '';
+	// 	for( let i = 0; i < addressArray.length; i++ ) {
+	// 		for( let i = 0; i < addressArray.length; i++ ) {
+	// 			if ( addressArray[ i ].types[0] && 'administrative_area_level_1' === addressArray[ i ].types[0] ) {
+	// 				state = addressArray[ i ].long_name;
+	// 				return state;
+	// 			}
+	// 		}
+	// 	}
+	// };
 
 	// And function for city,state and address input
 
@@ -128,15 +128,15 @@ class PetMap extends Component{
 		Geocode.fromLatLng( newLat , newLng ).then(
 			response => {
 				const address = response.results[0].formatted_address,
-					addressArray =  response.results[0].address_components,
-					city = this.getCity( addressArray ),
-					area = this.getArea( addressArray ),
-					state = this.getState( addressArray );
+					addressArray =  response.results[0].address_components;
+					// city = this.getCity( addressArray ),
+					// area = this.getArea( addressArray ),
+					// state = this.getState( addressArray );
 				this.setState( {
 					address: ( address ) ? address : '',
-					area: ( area ) ? area : '',
-					city: ( city ) ? city : '',
-					state: ( state ) ? state : ''
+					// area: ( area ) ? area : '',
+					// city: ( city ) ? city : '',
+					// state: ( state ) ? state : ''
 				} )
 			},
 			error => {
@@ -219,24 +219,24 @@ class PetMap extends Component{
 		let map;
 		if( this.props.center.lat !== undefined ) {
 			map = <div>
-				<div>
-					<div className="form-group">
+				{/* <div> */}
+					{/* <div className="form-group">
 						<label htmlFor="">City</label>
 						<input type="text" name="city" className="form-control" onChange={ this.onChange } readOnly="readOnly" value={ this.state.city }/>
 					</div>
 					<div className="form-group">
 						<label htmlFor="">Area</label>
 						<input type="text" name="area" className="form-control" onChange={ this.onChange } readOnly="readOnly" value={ this.state.area }/>
-					</div>
-					<div className="form-group">
+					</div> */}
+					{/* <div className="form-group">
 						<label htmlFor="">State</label>
 						<input type="text" name="state" className="form-control" onChange={ this.onChange } readOnly="readOnly" value={ this.state.state }/>
-					</div>
+					</div> */}
 					<div className="form-group">
 						<label htmlFor="">Address</label>
 						<input type="text" name="address" className="form-control" onChange={ this.onChange } readOnly="readOnly" value={ this.state.address }/>
 					</div>
-				</div>
+				{/* </div> */}
 
 				<AsyncMap
                     googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`}
