dcb2fbe19a9d85aa1c3cb93679b0327ee6d926a5
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-06-24 09:48:34 -0400

    Add search radius for each pet

--- client/src/Pets.js
+++ client/src/Pets.js
@@ -63,16 +63,16 @@ class Pets extends Component {
           //   </div>
           // </section>
           <section key={pet.id}>
-          
-          <article class="card">
-  <div class="image">
+
+          <article className="card">
+  <div className="image">
   <img key={pet.id} src={pet.picture} alt=""/>
   </div>
-  <div class="entry">
-    <div class="container">
-      <div class="text">
+  <div className="entry">
+    <div className="container">
+      <div className="text">
         <h1 className="card-title">{pet.name}</h1>
-        <span class="meta"> <TimeAgo date={pet.date_lost}/></span><span class="button button3">{pet.status}</span>
+        <span className="meta"> <TimeAgo date={pet.date_lost}/></span><span className="button button3">{pet.status}</span>
         <p>{pet.species}, {pet.description.breed} </p> <Link to={`/pets/${pet.id}`} className="btn btn-primary">more details</Link>
       </div>
     </div>
@@ -82,7 +82,7 @@ class Pets extends Component {
           )}
           </div>
           </InfiniteScroll>
-  
+
       );
     }
 };
