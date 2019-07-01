3823234a8e843d0f18c7d1d4f45d8f2655e93c98
JoshuaHarris93 <josh@emptychairproductions.com>
2019-07-01 15:59:21 +0000

    small style changes report a pet, pet profile

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,7 +4,7 @@ import Comment from './Comment';
 class CommentList extends Component {
     render() {
         let handleDelete = this.props.handleDelete;
-        const reverse = this.props.comments.reverse()
+        const reverse = this.props.comments
         const CommentNodes = reverse.map((comment, index)=>{
             return (
                 <Comment comment={comment.comment} key={index} current_user={this.props.current_user} user={comment.user} users={this.props.users} keyValue={index} handleDelete={handleDelete}>
