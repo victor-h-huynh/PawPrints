25c37b6c74ee5043174a847ac322d87662dbe5b5
JoshuaHarris93 <josh@emptychairproductions.com>
2019-07-02 18:18:21 +0000

    map positioning

--- client/src/Pets.js
+++ client/src/Pets.js
@@ -22,7 +22,7 @@ class Pets extends Component {
         <InfiniteScroll dataLength={this.state.items.length}
         next={this.fetchMoreData}
         hasMore={this.state.hasMore}
-        height={566}
+        height={550}
         endMessage={
           <p style={{ textAlign: "center" }}>
             <b>Yay! You have seen it all</b>
