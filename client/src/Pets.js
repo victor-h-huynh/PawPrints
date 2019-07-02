3168f8a3851be7c5160c8b21760b2a3b0c890167
JoshuaHarris93 <josh@emptychairproductions.com>
2019-07-02 18:49:15 +0000

    view height, card wrap styling

--- client/src/Pets.js
+++ client/src/Pets.js
@@ -22,7 +22,7 @@ class Pets extends Component {
         <InfiniteScroll dataLength={this.state.items.length}
         next={this.fetchMoreData}
         hasMore={this.state.hasMore}
-        height={550}
+        height={610}
         endMessage={
           <p style={{ textAlign: "center" }}>
             <b>Yay! You have seen it all</b>
