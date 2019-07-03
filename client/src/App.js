9e598ebf93b2f743209da61e7c08e09683dd6886
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-07-03 19:16:13 -0400

    Redo the redirect logic in users

--- client/src/App.js
+++ client/src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
     .then(res => {
       setTimeout(() => { this.setState({
         loading:false,
-      })}, 300)
+      })}, 3000)
     })
     .catch(error => console.log(error));
   }
